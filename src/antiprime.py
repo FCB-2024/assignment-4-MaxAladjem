f5271787aab82db9aec5b0c5c043d1a4b54ccb2b
MaxAladjem <max.aladjem01@estudiant.upf.edu>
2024-10-18 12:19:43 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,27 @@
-## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
-## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
-## COMMAND LINE SPECIFIED BELOW
-def main() :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+def main():
+    
+    def antiprimecalc(num):
+        i = 1
+        divnum = 0
+        divi = 0
+        divdef = 0
+        for i in range(1, num):
+            if num % i == 0:
+                divnum += 1
+        for i in range(1, num):
+            divi = 0
+            for j in range(1, i):
+                if i % j == 0:
+                    divi += 1
+            if divi >= divnum:
+                return "not antiprime"
+        return "antiprime"
+    num = int(input("Numero Positivo Entero?"))
+    result = antiprimecalc(num)
+    return result
 
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
 
-## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
 
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
-	## $ python antiprime.py 6
-	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
-	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
 	print(main())
